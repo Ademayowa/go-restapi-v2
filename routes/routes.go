b462d13ae9ab3af3db48fd65098ee9ad2ccd999e
ademayowa <ademayowaadeniyi@gmail.com>
2025-01-09 21:06:21 +0100

    chore(deps): added cors middleware

--- routes/routes.go
+++ routes/routes.go
@@ -1,8 +1,24 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterRoutes(server *gin.Engine) {
+	// Apply CORS middleware
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://job-board-v3.vercel.app"}, // Allow frontend domain
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,           // Set to true if you need cookies or authentication headers
+		MaxAge:           12 * time.Hour, // Cache preflight request for 12 hours
+	}))
+
+	// Define routes
 	server.GET("/jobs", getJobs)
 	server.POST("/jobs", createJob)
 	server.GET("/jobs/:id", getJob)
