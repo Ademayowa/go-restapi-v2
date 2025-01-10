304b90f682fc2d58782fd81fc89bcec56f412591
ademayowa <ademayowaadeniyi@gmail.com>
2025-01-10 10:55:11 +0100

    chore: testing a new service for auto deployment on render

--- routes/routes.go
+++ routes/routes.go
@@ -24,20 +24,3 @@ func RegisterRoutes(server *gin.Engine) {
 	server.POST("/jobs", createJob)
 	server.GET("/jobs/:id", getJob)
 }
-
-// func RegisterRoutes(server *gin.Engine) {
-// 	// Apply CORS middleware
-// 	server.Use(cors.New(cors.Config{
-// 		AllowOrigins:     []string{"https://job-board-v3.vercel.app"}, // Allow frontend domain
-// 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-// 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-// 		ExposeHeaders:    []string{"Content-Length"},
-// 		AllowCredentials: true,           // Set to true if you need cookies or authentication headers
-// 		MaxAge:           12 * time.Hour, // Cache preflight request for 12 hours
-// 	}))
-
-// 	// Define routes
-// 	server.GET("/jobs", getJobs)
-// 	server.POST("/jobs", createJob)
-// 	server.GET("/jobs/:id", getJob)
-// }
