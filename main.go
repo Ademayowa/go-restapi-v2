f3ee06a5e83bf3141b97e8a6ecf45a4e5dcc407a
ademayowa <ademayowaadeniyi@gmail.com>
2025-01-12 21:13:08 +0100

    chore: load .env dynamically

--- main.go
+++ main.go
@@ -3,14 +3,26 @@ package main
 import (
 	"job-board/db"
 	"job-board/routes"
-	
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load environment variables from .env file
+	if err := godotenv.Load(); err != nil {
+		println("No .env file found. Using default environment variables.")
+	}
+
 	db.InitDB()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
 	server := gin.Default()
 	routes.RegisterRoutes(server)
-	server.Run(":8080")
+	server.Run(":" + port)
 }
